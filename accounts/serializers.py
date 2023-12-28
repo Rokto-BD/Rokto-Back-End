00f50519839703a312c378320fe54c3e86260533
Taraldinn <kferdoush617@gmail.com>
2023-12-29 00:17:33 +0600

    initial commit

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,4 +8,3 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Account
         fields = ['username', 'email', 'first_name', 'last_name', 'phone_number', 'account_type', 'is_verified', 'account_created_at' ]
-
